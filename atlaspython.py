015013cd14faba146dd186284790291054b8c25f
Dj Walker-Morgan <djwm@mongodb.com>
2019-03-04 16:00:03 +0000

    Fixed up

---
+++ atlaspython.py
@@ -0,0 +1,11 @@
+import os
+import requests
+from requests.auth import HTTPDigestAuth
+import pprint
+
+baseurl = "https://cloud.mongodb.com/api/atlas/v1.0/"
+
+response=requests.get(baseurl, auth=HTTPDigestAuth(
+    os.environ["ATLAS_USER"], os.environ["ATLAS_USER_KEY"]))
+
+pprint.pprint(response.json())
