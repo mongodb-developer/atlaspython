8afbee2100ad850fb98ae053aa541b2d133d4808
Dj Walker-Morgan <djwm@mongodb.com>
2019-03-05 16:41:48 +0000

    SImpler version - inlined

--- atlaspython.py
+++ atlaspython.py
@@ -5,7 +5,7 @@ import pprint
 
 baseurl = "https://cloud.mongodb.com/api/atlas/v1.0/"
 
-response=requests.get(baseurl, auth=HTTPDigestAuth(
+response = requests.get(baseurl, auth=HTTPDigestAuth(
     os.environ["ATLAS_USER"], os.environ["ATLAS_USER_KEY"]))
 
 pprint.pprint(response.json())
